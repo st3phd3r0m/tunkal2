d7046cf8d041dc3f20174d959883098b2c5c3445
st3phd3r0m <stephane.derom@gmail.com>
2021-05-01 19:01:59 +0200

    essai sans adminlte

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,6 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
